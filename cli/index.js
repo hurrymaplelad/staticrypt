2f90482e2c235d6f8d3e8258b1de4c5f704f9373
Adam Hull <adam@hmlad.com>
2022-10-22 23:46:36 -0700

    Repair webcrypto imports

--- cli/index.js
+++ cli/index.js
@@ -1,31 +1,34 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
 const fs = require("fs");
 const path = require("path");
-const Yargs = require('yargs');
-const { subtle, getRandomValues } = require('crypto').webcrypto;
+const Yargs = require("yargs");
+const { subtle, getRandomValues } = require("crypto").webcrypto;
 const { readFileSync, writeFileSync } = require("fs");
 const { join } = require("path");
-const { usage, showHelp } = require('yargs');
+const { usage, showHelp } = require("yargs");
+const { webcrypto } = require("crypto");
 
+const getRandomValues = webcrypto.getRandomValues.bind(webcrypto);
+const subtle = webcrypto.subtle;
 
 /**
  * Salt and encrypt a msg with a password.
  * Inspired by https://github.com/adonespitogo
  */
 function encrypt(msg, hashedPassphrase) {
-    var iv = CryptoJS.lib.WordArray.random(128 / 8);
-    var encrypted = CryptoJS.AES.encrypt(msg, hashedPassphrase, {
-        iv: iv,
-        padding: CryptoJS.pad.Pkcs7,
-        mode: CryptoJS.mode.CBC
-    });
+  var iv = CryptoJS.lib.WordArray.random(128 / 8);
+  var encrypted = CryptoJS.AES.encrypt(msg, hashedPassphrase, {
+    iv: iv,
+    padding: CryptoJS.pad.Pkcs7,
+    mode: CryptoJS.mode.CBC,
+  });
 
-    // iv will be hex 16 in length (32 characters)
-    // we prepend it to the ciphertext for use in decryption
-    return iv.toString() + encrypted.toString();
+  // iv will be hex 16 in length (32 characters)
+  // we prepend it to the ciphertext for use in decryption
+  return iv.toString() + encrypted.toString();
 }
 
 /**
@@ -36,16 +39,16 @@ function encrypt(msg, hashedPassphrase) {
  * @returns string
  */
 function hashPassphrase(passphrase, salt) {
-    var hashedPassphrase = CryptoJS.PBKDF2(passphrase, salt, {
-        keySize: 256 / 32,
-        iterations: 1000
-    });
+  var hashedPassphrase = CryptoJS.PBKDF2(passphrase, salt, {
+    keySize: 256 / 32,
+    iterations: 1000,
+  });
 
-    return hashedPassphrase.toString();
+  return hashedPassphrase.toString();
 }
 
 function generateRandomSalt() {
-    return CryptoJS.lib.WordArray.random(128 / 8).toString();
+  return CryptoJS.lib.WordArray.random(128 / 8).toString();
 }
 
 /**
@@ -61,120 +64,121 @@ function generateRandomSalt() {
  * @returns {boolean}
  */
 function isOptionSetByUser(option, yargs) {
-    function searchForOption(option) {
-        return process.argv.indexOf(option) > -1;
-    }
+  function searchForOption(option) {
+    return process.argv.indexOf(option) > -1;
+  }
 
-    if (searchForOption(`-${option}`) || searchForOption(`--${option}`)) {
-        return true;
-    }
+  if (searchForOption(`-${option}`) || searchForOption(`--${option}`)) {
+    return true;
+  }
 
-    // Handle aliases for same option
-    for (let aliasIndex in yargs.parsed.aliases[option]) {
-        const alias = yargs.parsed.aliases[option][aliasIndex];
+  // Handle aliases for same option
+  for (let aliasIndex in yargs.parsed.aliases[option]) {
+    const alias = yargs.parsed.aliases[option][aliasIndex];
 
-        if (searchForOption(`-${alias}`) || searchForOption(`--${alias}`))
-            return true;
-    }
+    if (searchForOption(`-${alias}`) || searchForOption(`--${alias}`))
+      return true;
+  }
 
-    return false;
+  return false;
 }
 
-const yargs = Yargs
-    .usage('Usage: staticrypt <filename> <passphrase> [options]')
-    .option('c', {
-        alias: 'config',
-        type: 'string',
-        describe: 'Path to the config file. Set to "false" to disable.',
-        default: '.staticrypt.json',
-    })
-    .option('decrypt-button', {
-        type: 'string',
-        describe: 'Label to use for the decrypt button. Default: "DECRYPT".',
-        default: 'DECRYPT'
-    })
-    .option('e', {
-        alias: 'embed',
-        type: 'boolean',
-        describe: 'Whether or not to embed crypto-js in the page (or use an external CDN).',
-        default: true
-    })
-    .option('f', {
-        alias: 'file-template',
-        type: 'string',
-        describe: 'Path to custom HTML template with passphrase prompt.',
-        default: path.join(__dirname, 'password_template.html')
-    })
-    .option('i', {
-        alias: 'instructions',
-        type: 'string',
-        describe: 'Special instructions to display to the user.',
-        default: ''
-    })
-    .option('noremember', {
-        type: 'boolean',
-        describe: 'Set this flag to remove the "Remember me" checkbox.',
-        default: false,
-    })
-    .option('o', {
-        alias: 'output',
-        type: 'string',
-        describe: 'File name / path for generated encrypted file.',
-        default: null
-    })
-    .option('passphrase-placeholder', {
-        type: 'string',
-        describe: 'Placeholder to use for the passphrase input.',
-        default: 'Passphrase'
-    })
-    .option('r', {
-        alias: 'remember',
-        type: 'number',
-        describe: 'Expiration in days of the "Remember me" checkbox that will save the (salted + hashed) passphrase ' +
-            'in localStorage when entered by the user. Default: "0", no expiration.',
-        default: 0,
-    })
-    .option('remember-label', {
-        type: 'string',
-        describe: 'Label to use for the "Remember me" checkbox.',
-        default: 'Remember me'
-    })
-    // do not give a default option to this 'remember' parameter - we want to see when the flag is included with no
-    // value and when it's not included at all
-    .option('s', {
-        alias: 'salt',
-        describe: 'Set the salt manually. It should be set if you want use "Remember me" through multiple pages. It ' +
-            'needs to be a 32 character long hexadecimal string.\nInclude the empty flag to generate a random salt you ' +
-            'can use: "statycrypt -s".',
-        type: 'string',
-    })
-    .option('t', {
-        alias: 'title',
-        type: 'string',
-        describe: "Title for output HTML page.",
-        default: 'Protected Page'
-    });
+const yargs = Yargs.usage("Usage: staticrypt <filename> <passphrase> [options]")
+  .option("c", {
+    alias: "config",
+    type: "string",
+    describe: 'Path to the config file. Set to "false" to disable.',
+    default: ".staticrypt.json",
+  })
+  .option("decrypt-button", {
+    type: "string",
+    describe: 'Label to use for the decrypt button. Default: "DECRYPT".',
+    default: "DECRYPT",
+  })
+  .option("e", {
+    alias: "embed",
+    type: "boolean",
+    describe:
+      "Whether or not to embed crypto-js in the page (or use an external CDN).",
+    default: true,
+  })
+  .option("f", {
+    alias: "file-template",
+    type: "string",
+    describe: "Path to custom HTML template with passphrase prompt.",
+    default: path.join(__dirname, "password_template.html"),
+  })
+  .option("i", {
+    alias: "instructions",
+    type: "string",
+    describe: "Special instructions to display to the user.",
+    default: "",
+  })
+  .option("noremember", {
+    type: "boolean",
+    describe: 'Set this flag to remove the "Remember me" checkbox.',
+    default: false,
+  })
+  .option("o", {
+    alias: "output",
+    type: "string",
+    describe: "File name / path for generated encrypted file.",
+    default: null,
+  })
+  .option("passphrase-placeholder", {
+    type: "string",
+    describe: "Placeholder to use for the passphrase input.",
+    default: "Passphrase",
+  })
+  .option("r", {
+    alias: "remember",
+    type: "number",
+    describe:
+      'Expiration in days of the "Remember me" checkbox that will save the (salted + hashed) passphrase ' +
+      'in localStorage when entered by the user. Default: "0", no expiration.',
+    default: 0,
+  })
+  .option("remember-label", {
+    type: "string",
+    describe: 'Label to use for the "Remember me" checkbox.',
+    default: "Remember me",
+  })
+  // do not give a default option to this 'remember' parameter - we want to see when the flag is included with no
+  // value and when it's not included at all
+  .option("s", {
+    alias: "salt",
+    describe:
+      'Set the salt manually. It should be set if you want use "Remember me" through multiple pages. It ' +
+      "needs to be a 32 character long hexadecimal string.\nInclude the empty flag to generate a random salt you " +
+      'can use: "statycrypt -s".',
+    type: "string",
+  })
+  .option("t", {
+    alias: "title",
+    type: "string",
+    describe: "Title for output HTML page.",
+    default: "Protected Page",
+  });
 const namedArgs = yargs.argv;
 
-
 // if the 's' flag is passed without parameter, generate a salt, display & exit
-if (isOptionSetByUser('s', yargs) && !namedArgs.salt) {
-    console.log(generateRandomSalt());
-    process.exit(0);
+if (isOptionSetByUser("s", yargs) && !namedArgs.salt) {
+  console.log(generateRandomSalt());
+  process.exit(0);
 }
 
 // validate the number of arguments
 if (namedArgs._.length !== 2) {
-    Yargs.showHelp();
-    process.exit(1);
+  Yargs.showHelp();
+  process.exit(1);
 }
 
 // get config file
-const isUsingconfigFile = namedArgs.config.toLowerCase() !== 'false';
-const configPath = './' + namedArgs.config;
+const isUsingconfigFile = namedArgs.config.toLowerCase() !== "false";
+const configPath = "./" + namedArgs.config;
 let config = {};
 if (isUsingconfigFile && fs.existsSync(configPath)) {
-    config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+  config = JSON.parse(fs.readFileSync(configPath, "utf8"));
 }
 
 /**
@@ -183,41 +187,43 @@ if (isUsingconfigFile && fs.existsSync(configPath)) {
 let salt;
 // either a salt was provided by the user through the flag --salt
 if (!!namedArgs.salt) {
-    salt = String(namedArgs.salt).toLowerCase();
+  salt = String(namedArgs.salt).toLowerCase();
 }
 // or we try to read the salt from config file
 else if (!!config.salt) {
-    salt = config.salt;
+  salt = config.salt;
 }
 // or we generate a salt
 else {
-    salt = generateRandomSalt();
+  salt = generateRandomSalt();
 }
 
 // validate the salt
 if (salt.length !== 32 || /[^a-f0-9]/.test(salt)) {
-    console.log("The salt should be a 32 character long hexadecimal string (only [0-9a-f] characters allowed)");
-    console.log("Detected salt: " + salt);
-    process.exit(1);
+  console.log(
+    "The salt should be a 32 character long hexadecimal string (only [0-9a-f] characters allowed)"
+  );
+  console.log("Detected salt: " + salt);
+  process.exit(1);
 }
 
 // write salt to config file
 if (isUsingconfigFile && config.salt !== salt) {
-    config.salt = salt;
-    fs.writeFileSync(configPath, JSON.stringify(config, null, 4));
+  config.salt = salt;
+  fs.writeFileSync(configPath, JSON.stringify(config, null, 4));
 }
 
 // parse input
 const input = namedArgs._[0].toString(),
-    passphrase = namedArgs._[1].toString();
+  passphrase = namedArgs._[1].toString();
 
 // get the file content
 let contents;
 try {
-    contents = fs.readFileSync(input, 'utf8');
+  contents = fs.readFileSync(input, "utf8");
 } catch (e) {
-    console.log("Failure: input file does not exist!");
-    process.exit(1);
+  console.log("Failure: input file does not exist!");
+  process.exit(1);
 }
 
 // encrypt input
@@ -228,64 +234,73 @@ const encrypted = encrypt(contents, hashedPassphrase);
 // const hmac = CryptoJS.HmacSHA256(encrypted, CryptoJS.SHA256(hashedPassphrase).toString()).toString();
 // const encryptedMessage = hmac + encrypted;
 
-
 // TODO(aph) - Merge this data with the literal in ecrypt below
 const data = {
-    decrypt_button: namedArgs.decryptButton,
-    embed: namedArgs.embed,
-    encrypted: encryptedMessage,
-    instructions: namedArgs.instructions,
-    is_remember_enabled: namedArgs.noremember ? 'false' : 'true',
-    output_file_path: namedArgs.output !== null ? namedArgs.output : input.replace(/\.html$/, '') + "_encrypted.html",
-    passphrase_placeholder: namedArgs.passphrasePlaceholder,
-    remember_duration_in_days: namedArgs.remember,
-    remember_me: namedArgs.rememberLabel,
-    salt: salt,
-    title: namedArgs.title,
+  decrypt_button: namedArgs.decryptButton,
+  embed: namedArgs.embed,
+  encrypted: encryptedMessage,
+  instructions: namedArgs.instructions,
+  is_remember_enabled: namedArgs.noremember ? "false" : "true",
+  output_file_path:
+    namedArgs.output !== null
+      ? namedArgs.output
+      : input.replace(/\.html$/, "") + "_encrypted.html",
+  passphrase_placeholder: namedArgs.passphrasePlaceholder,
+  remember_duration_in_days: namedArgs.remember,
+  remember_me: namedArgs.rememberLabel,
+  salt: salt,
+  title: namedArgs.title,
 };
 
 // genFile(data);
 
+function encrypt(msg, password) {
+  var iv = getRandomValues(new Uint8Array(16));
+  var ivHex = bytesToHexString(iv);
+  var pwUtf8 = stringToUint8Array(password);
 
-function encrypt (msg, password) {
-    var iv = getRandomValues(new Uint8Array(16));
-    var ivHex = bytesToHexString(iv);
-    var pwUtf8 = stringToUint8Array(password);
-
-    subtle.digest('SHA-256', pwUtf8)
-    .then(function(hash) {
-        return bytesToHexString(hash);
+  subtle
+    .digest("SHA-256", pwUtf8)
+    .then(function (hash) {
+      return bytesToHexString(hash);
     })
-    .then(function(pwHex) {
-    return hexStringToUint8Array(pwHex);
+    .then(function (pwHex) {
+      return hexStringToUint8Array(pwHex);
     })
-    .then(function(keyData) {
-    return subtle.importKey("raw", keyData, "AES-GCM", false, ["encrypt"]);
+    .then(function (keyData) {
+      return subtle.importKey("raw", keyData, "AES-GCM", false, ["encrypt"]);
     })
-    .then(function(key) {
-        return subtle.encrypt({
-            name: "AES-GCM",
-            iv: iv
-        }, key, stringToUint8Array(msg));
+    .then(function (key) {
+      return subtle.encrypt(
+        {
+          name: "AES-GCM",
+          iv: iv,
+        },
+        key,
+        stringToUint8Array(msg)
+      );
     })
-    .then(function(cipherText) {
-        return ivHex + bytesToHexString(cipherText);
+    .then(function (cipherText) {
+      return ivHex + bytesToHexString(cipherText);
     })
-    .then(function(encrypted) {
-        return {
-            title: namedArgs.title,
-            instructions: namedArgs.instructions,
-            encrypted: encrypted,
-            outputFilePath: namedArgs.output !== null ? namedArgs.output : input.replace(/\.html$/, '') + "_encrypted.html"
-        };
+    .then(function (encrypted) {
+      return {
+        title: namedArgs.title,
+        instructions: namedArgs.instructions,
+        encrypted: encrypted,
+        outputFilePath:
+          namedArgs.output !== null
+            ? namedArgs.output
+            : input.replace(/\.html$/, "") + "_encrypted.html",
+      };
     })
-    .then(function(data) {
-        genFile(data);
+    .then(function (data) {
+      genFile(data);
     })
     .catch(console.log);
 }
 
-encrypt(contents, password)
+encrypt(contents, password);
 
 /**
  * Fill the template with provided data and writes it to output file.
@@ -293,23 +308,23 @@ encrypt(contents, password)
  * @param data
  */
 function genFile(data) {
-    let templateContents;
+  let templateContents;
 
-    try {
-        templateContents = fs.readFileSync(namedArgs.f, 'utf8');
-    } catch (e) {
-        console.log("Failure: could not read template!");
-        process.exit(1);
-    }
+  try {
+    templateContents = fs.readFileSync(namedArgs.f, "utf8");
+  } catch (e) {
+    console.log("Failure: could not read template!");
+    process.exit(1);
+  }
 
-    const renderedTemplate = render(templateContents, data);
+  const renderedTemplate = render(templateContents, data);
 
-    try {
-        fs.writeFileSync(data.output_file_path, renderedTemplate);
-    } catch (e) {
-        console.log("Failure: could not generate output file!");
-        process.exit(1);
-    }
+  try {
+    fs.writeFileSync(data.output_file_path, renderedTemplate);
+  } catch (e) {
+    console.log("Failure: could not generate output file!");
+    process.exit(1);
+  }
 }
 
 /**
@@ -320,51 +335,47 @@ function genFile(data) {
  * @returns string
  */
 function render(tpl, data) {
-    return tpl.replace(/{(.*?)}/g, function (_, key) {
-        if (data && data[key] !== undefined) {
-            return data[key];
-        }
+  return tpl.replace(/{(.*?)}/g, function (_, key) {
+    if (data && data[key] !== undefined) {
+      return data[key];
+    }
 
-        return '';
-    });
+    return "";
+  });
 }
 
 /*
-* encode/decode funtions
-*/
+ * encode/decode funtions
+ */
 
 function hexStringToUint8Array(hexString) {
-    if (hexString.length % 2 != 0)
-        throw "Invalid hexString";
-    var arrayBuffer = new Uint8Array(hexString.length / 2);
+  if (hexString.length % 2 != 0) throw "Invalid hexString";
+  var arrayBuffer = new Uint8Array(hexString.length / 2);
 
-    for (var i = 0; i < hexString.length; i += 2) {
-        var byteValue = parseInt(hexString.substr(i, 2), 16);
-        if (byteValue == NaN)
-            throw "Invalid hexString";
-        arrayBuffer[i / 2] = byteValue;
-    }
+  for (var i = 0; i < hexString.length; i += 2) {
+    var byteValue = parseInt(hexString.substr(i, 2), 16);
+    if (byteValue == NaN) throw "Invalid hexString";
+    arrayBuffer[i / 2] = byteValue;
+  }
 
-    return arrayBuffer;
+  return arrayBuffer;
 }
 
 function bytesToHexString(bytes) {
-    if (!bytes)
-        return null;
+  if (!bytes) return null;
 
-    bytes = new Uint8Array(bytes);
-    var hexBytes = [];
+  bytes = new Uint8Array(bytes);
+  var hexBytes = [];
 
-    for (var i = 0; i < bytes.length; ++i) {
-        var byteString = bytes[i].toString(16);
-        if (byteString.length < 2)
-            byteString = "0" + byteString;
-        hexBytes.push(byteString);
-    }
-    return hexBytes.join("");
+  for (var i = 0; i < bytes.length; ++i) {
+    var byteString = bytes[i].toString(16);
+    if (byteString.length < 2) byteString = "0" + byteString;
+    hexBytes.push(byteString);
+  }
+  return hexBytes.join("");
 }
 
 function stringToUint8Array(str) {
-    var encoder = new TextEncoder('utf-8');
-    return encoder.encode(str);
+  var encoder = new TextEncoder("utf-8");
+  return encoder.encode(str);
 }
